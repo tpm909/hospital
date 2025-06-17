e34761bed93bafa7c216ef8c48aa5bb61262dea0
tpm909 <ankitoperez909@gmail.com>
2025-06-17 20:21:27 -0300

    preparacion para el deply

--- Model/db.js
+++ Model/db.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-
-const  sequelize  = new Sequelize(
-    'hospitalbd',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        logging: false
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: process.env.DB_LOGGING === 'true',  
+  }
 );
 
-module.exports = sequelize
+module.exports = sequelize;
